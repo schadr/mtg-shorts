2be8d41b2c8195063f9c496528b4a7a37b6a87b0
Adrian Schroeter <aschroeter@vmware.com>
2025-06-26 19:06:46 +0000

    Added functionality to download and setup the OCR library

--- src/video.py
+++ src/video.py
@@ -1,4 +1,7 @@
 import cv2 as cv
+import urllib.request
+import tarfile
+import io
 
 def load_video(file_uri):
     vid = cv.VideoCapture(file_uri)
@@ -16,3 +19,21 @@ def extract_card_info(video):
             break
         print(f"Read frame {i}")
         i += 1
+
+east_uri = 'https://www.dropbox.com/s/r2ingd0l3zt8hxs/frozen_east_text_detection.tar.gz?dl=1'
+
+def load_text_detection(inputSize):
+    with urllib.request.urlopen(east_uri) as response:
+            with tarfile.open(fileobj=response, mode="r:gz") as tar:
+                tar.extractall(path=".")
+    
+    # values might need tuning
+    conf_thresh = 0.8
+    nms_thresh = 0.4
+    mean = (122.67891434, 116.66876762, 104.00698793)
+    
+    textDetectorEAST = cv.dnn_TextDetectionModel_EAST('./frozen_east_text_detection.pb')
+    textDetectorEAST.setConfidenceThreshold(conf_thresh)
+    textDetectorEAST.setNMSThreshold(nms_thresh)
+    textDetectorEAST.setInputParams(1.0, inputSize, mean, True)
+    return textDetectorEAST
--- tests/test_priceextraction.py
+++ tests/test_priceextraction.py
@@ -1,5 +1,6 @@
 from src.video import load_video
 from src.video import extract_card_info
+from src.video import load_text_detection
 
 def test_load_video():
     assert load_video('files/test-video-720p.mov') != None
@@ -7,3 +8,6 @@ def test_load_video():
 def test_extract_card_info():
     video = load_video('files/test-video-720p.mov')
     extract_card_info(video)
+
+def test_load_text_dectection():
+    assert load_text_detection((320, 320)) != None # update the size
