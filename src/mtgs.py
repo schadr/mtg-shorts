e12f2f9a837dba05b2a106cef0cf21cbbf500f9e
Adrian Schroeter <aschroeter@vmware.com>
2025-07-04 04:04:35 +0000

    Added disinction for collector packs to generate fps videos
    closes #14

--- src/mtgs.py
+++ src/mtgs.py
@@ -9,20 +9,23 @@ from src.caption_generation import smooth_captions
 from src.pricing import Card
 from src.video import add_card_info_to_video, extract_card_info_from_video, load_video
 
-def process_video(file_path):
+def process_video(file_path, collector=False):
     video = load_video(file_path)
     cards_in_frame = extract_card_info_from_video(video)
     smoothed_captions = smooth_captions(cards_in_frame)
     add_card_info_to_video(video, smoothed_captions)
 
-def create_fps_video(file_path):
+def create_fps_video(file_path, collector=False):
     video = load_video(file_path)
     num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
     cards_in_frame = [Card("uuid", f"Frame: {i}", i, "set", i, i) for i in range(num_frames)]
     filename = os.path.basename(file_path)
     path = os.path.dirname(file_path)
     add_card_info_to_video(video, cards_in_frame, f"{path}/frame-{filename}", 1)
-    shutil.copyfile("templates/template-play-booster.json", f"{path}/cards-{filename.replace(".","-")}.json")
+    source = "templates/template-play-booster.json"
+    if collector:
+        source = "templates/template-collector-booster.json" 
+    shutil.copyfile(source, f"{path}/cards-{filename.replace(".","-")}.json")
 
 
 def main():
@@ -32,6 +35,7 @@ def main():
     group.add_argument('--file', type=str, help='Path to the video file that needs to be edited')
     group.add_argument('--folder', type=str, help='Folder containing video files to be processed')
     group.add_argument('--fps', type=bool, help='Outputs videos with with framenumber captions at 1 fps')
+    group.add_argument('--collector', type=bool, help='Video(s) are of collector packs')
     args = parser.parse_args()
 
     processor = None
@@ -41,13 +45,13 @@ def main():
         processor = process_video
 
     if args.file:
-        processor(args.file)
+        processor(args.file, args.collector)
         
     if args.folder:
         for item in os.listdir(args.folder):
             file_name = os.path.join(args.folder, item)
             if os.path.isfile(file_name):
-                processor(os.path.join(args.folder, file_name))
+                processor(os.path.join(args.folder, file_name), args.collector)
 
 if __name__ == "__main__":
     main()
--- tests/test_mtgs.py
+++ tests/test_mtgs.py
@@ -4,3 +4,10 @@ from src.mtgs import create_fps_video
 
 def test_create_fps_video():
     create_fps_video("files/test-video-720p.mov")
+    assert os.path.exists("files/frame-test-video-720p.mov")
+    assert os.path.exists("files/cards-test-video-720p-mov.json")
+
+def test_create_fps_video_collector():
+    create_fps_video("files/test-video-720p.mov", True)
+    assert os.path.exists("files/frame-test-video-720p.mov")
+    assert os.path.exists("files/cards-test-video-720p-mov.json")
