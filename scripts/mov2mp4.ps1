25c5d284e16b0f55c5e57f3f6a62e4aef738596c
schadr <adrian.schrtoeter@gmail.com>
2025-09-16 15:00:35 -0700

    added paramter for folder with mov files to convert

--- scripts/mov2mp4.ps1
+++ scripts/mov2mp4.ps1
@@ -1,4 +1,14 @@
 # This file converts all MOV files in a folder into mp4's ussing ffmpeg
 # rotate ffmpeg -i input.mp4 -vf "transpose=1" output.mp4
 
-Get-Childitem *.mov | foreach-object -Parallel { ffmpeg -i $_.Name -qscale 0 $_.Name.Replace('.MOV','.mp4').Replace('.mov','.mp4') } -ThrottleLimit 3
+param(
+    [string]$Folder = "."
+)
+Push-Location $Folder
+
+try {
+    Get-Childitem *.mov | foreach-object -Parallel { ffmpeg -i $_.Name -qscale 0 $_.Name.Replace('.MOV','.mp4').Replace('.mov','.mp4') } -ThrottleLimit 3
+}
+finally {
+    Pop-Location
+}
